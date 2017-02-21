74d6ff5347e8f618b01c73d417d082b6f8f293d6
Crystal Hess <crystal@adadevelopersacademy.org>
2017-02-21 14:34:16 -0800

    restructures folders

---
+++ lib/Queue.rb
@@ -0,0 +1,19 @@
+class Queue
+  def initialize
+  end
+  
+  def enqueue(element)
+  end
+ 
+  def dequeue
+  end
+
+  def front
+  end
+ 
+  def size
+  end
+
+  def empty?
+  end
+end
---
+++ lib/Stack.rb
@@ -0,0 +1,20 @@
+class Stack
+  def initialize
+    @store = Array.new
+  end
+  
+  def push(element)
+  end
+ 
+  def pop
+  end
+
+  def top
+  end
+ 
+  def size
+  end
+
+  def empty?
+  end
+end
