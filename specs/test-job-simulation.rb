53faa8ee6a8c770bc55b14e3a15c0aa5d3c776c2
Crystal Hess <crystal@adadevelopersacademy.org>
2017-02-22 23:19:21 -0800

    adds empty testing file for job simulation

---
+++ specs/test-job-simulation.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative '../lib/job-simulation'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe "Test Terrible Company" do
+
+end
