af5ab68b455917bbf849abd811716f1d3be24c06
Crystal Hess <crystal@adadevelopersacademy.org>
2017-02-22 21:05:50 -0800

    adds to_s method to be defined in assignment

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,19 +1,22 @@
 class Queue
   def initialize
   end
-  
+
   def enqueue(element)
   end
- 
+
   def dequeue
   end
 
   def front
   end
- 
+
   def size
   end
 
   def empty?
+  end
+
+  def to_s
   end
 end
