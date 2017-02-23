0e5f02f8e4889da002be7ee1364ad89703ff4206
Crystal Hess <crystal@adadevelopersacademy.org>
2017-02-22 21:22:18 -0800

    adds coloring to tests and fixes skips

--- specs/test-stack-implementation.rb
+++ specs/test-stack-implementation.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/Stack'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Test Stack Implementation" do
   it "creates a Stack" do
@@ -9,14 +10,14 @@ describe "Test Stack Implementation" do
   end
 
   it "pushes something onto a empty Stack" do
-    # skip
+    skip
     s = Stack.new
     s.push(10)
     s.to_s.must_equal "[10]"
   end
 
   it "pushes multiple somethings onto a Stack" do
-    # skip
+    skip
     s = Stack.new
     s.push(10)
     s.push(20)
@@ -25,14 +26,14 @@ describe "Test Stack Implementation" do
   end
 
   it "starts the size of a Stack at 0" do
-    # skip
+    skip
     s = Stack.new
     s.size.must_equal 0
     s.empty?.must_equal true
   end
 
   it "removes something from the stack" do
-    # skip
+    skip
     s = Stack.new
     s.push(5)
     s.pop
@@ -41,7 +42,7 @@ describe "Test Stack Implementation" do
   end
 
   it "removes the right something (LIFO)" do
-    # skip
+    skip
     s = Stack.new
     s.push(5)
     s.push(3)
@@ -52,7 +53,7 @@ describe "Test Stack Implementation" do
   end
 
   it "properly adjusts the size with pushing and poping" do
-    # skip
+    skip
     s = Stack.new
     s.empty?.must_equal true
     s.push(-1)
@@ -67,7 +68,7 @@ describe "Test Stack Implementation" do
   end
 
   it "returns the top element on the stack" do
-    # skip
+    skip
     s = Stack.new
     s.push(40)
     s.push(22)
@@ -76,6 +77,6 @@ describe "Test Stack Implementation" do
 
   # Challege Tests you could write yourself:
   # it "doesn't alter the size when you call top" do
-  # it "raises an exception if you try to pop from an empty stack" do
-  # it "raises an exception if you try to call top on an empty stack" do
+  # it "raises an error if you try to pop from an empty stack" do
+  # it "raises an error if you try to call top on an empty stack" do
 end
