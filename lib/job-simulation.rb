570519f1734baca7f91ade6dfa488b5259641c6c
Crystal Hess <crystal@adadevelopersacademy.org>
2017-02-23 00:00:42 -0800

    provides skeleton for job simulation program

--- lib/job-simulation.rb
+++ lib/job-simulation.rb
@@ -1,15 +1,31 @@
-# A company has six hiring positions with more people wanting jobs than the
-# number of available positions.  The company managers decide in order to give
-# more people an opportunity to make money; they will allow people to work in
-# three-month intervals. The first five people on the waiting list will be hired
-# in the order that they are on the waiting list.  The first six people will
-# keep these positions for three months.  At the end of three months, the
-# manager will roll a dice to determine the number of people who will lose their
-# jobs. The company will use the policy of last-hired-first-fired.  For example,
-# if the dice roll is 3, the last 3 people hired will lose their jobs to the
-# first 3 people on the waiting list. People losing their jobs will be placed on
-# the back of the waiting list in the order that they are fired. This process
-# will continue for every three-month interval.
-
 require './Stack.rb'
 require './Queue.rb'
+
+class JobSimulation
+  attr_reader :workers, :waiting, :roll
+
+  def initialize (jobs_available, job_seekers)
+
+  end
+
+  def cycle
+
+  end
+end
+
+## Allows us to run our code and see what's happening:
+sim = JobSimulation.new(6,10)
+puts "------------------------------"
+puts "Before the simulation starts"
+puts "Employed: #{sim.workers}"
+puts "Waitlist: #{sim.waiting}"
+puts "------------------------------"
+print "<enter to cycle>\n"
+
+count = 0
+until gets.chomp != ""
+  puts "-------Cycle #{count+=1}-------"
+  sim.cycle
+  puts "Employed: #{sim.workers}"
+  puts "Waitlist: #{sim.waiting}"
+end
