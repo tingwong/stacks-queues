34f6970c390a34636c32b4cf7844222c45fdc705
Ting Wong <hellotingwong@gmail.com>
2017-02-26 17:06:58 -0800

    Queue Homework

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store << element
   end
 
   def dequeue
+    return @store.shift
   end
 
   def front
+    return @store.first
   end
 
   def size
+     return @store.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
+    return @store.to_s
   end
 end
